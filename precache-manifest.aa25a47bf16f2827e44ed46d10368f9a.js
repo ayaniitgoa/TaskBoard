5dfc29724a9050c13edeeb8db1936af218af0bd9
Ayan <ayan.adhya.18003@iitgoa.ac.in>
2020-10-06 18:00:05 +0530

    Updates

---
+++ precache-manifest.aa25a47bf16f2827e44ed46d10368f9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6990ecd59732dca4314b0ab99d485bb6",
+    "url": "/TaskBoard/index.html"
+  },
+  {
+    "revision": "573793e30a79deb75688",
+    "url": "/TaskBoard/static/css/main.db5fb6c9.chunk.css"
+  },
+  {
+    "revision": "afdc025b85efa0afbbc5",
+    "url": "/TaskBoard/static/js/2.b7504d41.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/TaskBoard/static/js/2.b7504d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "573793e30a79deb75688",
+    "url": "/TaskBoard/static/js/main.62f72451.chunk.js"
+  },
+  {
+    "revision": "6a94af1804aced13d770",
+    "url": "/TaskBoard/static/js/runtime-main.f73694fe.js"
+  }
+]);
